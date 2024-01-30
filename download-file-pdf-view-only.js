40e1a761ff1731ecd2b1aa4ee3c13aeb65f47a17
Truong Pham <truongpham@Truongs-MacBook.local>
2024-01-31 02:36:46 +0700

    note: load all file pdf then paste code to devtools

--- download-file-pdf-view-only.js
+++ download-file-pdf-view-only.js
@@ -10,109 +10,31 @@ if (window.trustedTypes && trustedTypes.createPolicy) {
     trustedURL = 'https://cdnjs.cloudflare.com/ajax/libs/jspdf/1.3.2/jspdf.min.js';
 }
 
+// Load the jsPDF library using the trusted URL.
 let jspdf = document.createElement("script");
-
 jspdf.onload = function () {
-
+    // Generate a PDF from images with "blob:" sources.
     let pdf = new jsPDF();
     let elements = document.getElementsByTagName("img");
-    for (let i in elements) {
+    for (let i = 0; i < elements.length; i++) {
         let img = elements[i];
-        console.log("add img ", img);
         if (!/^blob:/.test(img.src)) {
-            console.log("invalid src");
             continue;
         }
-        let can = document.createElement('canvas');
-        let con = can.getContext("2d");
-        can.width = img.width;
-        can.height = img.height;
+        let canvasElement = document.createElement('canvas');
+        let con = canvasElement.getContext("2d");
+        canvasElement.width = img.width;
+        canvasElement.height = img.height;
         con.drawImage(img, 0, 0, img.width, img.height);
-        let imgData = can.toDataURL("image/jpeg", 1.0);
+        let imgData = canvasElement.toDataURL("image/jpeg", 1.0);
         pdf.addImage(imgData, 'JPEG', 0, 0);
-        pdf.addPage();
+        if (i !== elements.length - 1) {
+            pdf.addPage();
+        }
     }
 
+    // Download the generated PDF.
     pdf.save("download.pdf");
 };
-
-jspdf.src = 'https://drive.google.com/file/d/10su27mqZi6ipykZ4_5oEIjqWp8MCNn03/view'; /* had to set it like this, because disqus was breaking the link.. */
-document.body.appendChild(jspdf);let trustedURL;
-if (window.trustedTypes && trustedTypes.createPolicy) {
-    const policy = trustedTypes.createPolicy('myPolicy', {
-        createScriptURL: (input) => {
-            return input;
-        }
-    });
-    trustedURL = policy.createScriptURL('https://cdnjs.cloudflare.com/ajax/libs/jspdf/1.3.2/jspdf.min.js');
-} else {
-    trustedURL = 'https://cdnjs.cloudflare.com/ajax/libs/jspdf/1.3.2/jspdf.min.js';
-}
-
-let jspdf = document.createElement("script");
-
-jspdf.onload = function () {
-
-    let pdf = new jsPDF();
-    let elements = document.getElementsByTagName("img");
-    for (let i in elements) {
-        let img = elements[i];
-        console.log("add img ", img);
-        if (!/^blob:/.test(img.src)) {
-            console.log("invalid src");
-            continue;
-        }
-        let can = document.createElement('canvas');
-        let con = can.getContext("2d");
-        can.width = img.width;
-        can.height = img.height;
-        con.drawImage(img, 0, 0, img.width, img.height);
-        let imgData = can.toDataURL("image/jpeg", 1.0);
-        pdf.addImage(imgData, 'JPEG', 0, 0);
-        pdf.addPage();
-    }
-
-    pdf.save("download.pdf");
-};
-
-jspdf.src = 'https://drive.google.com/file/d/10su27mqZi6ipykZ4_5oEIjqWp8MCNn03/view'; /* had to set it like this, because disqus was breaking the link.. */
-document.body.appendChild(jspdf);let trustedURL;
-if (window.trustedTypes && trustedTypes.createPolicy) {
-    const policy = trustedTypes.createPolicy('myPolicy', {
-        createScriptURL: (input) => {
-            return input;
-        }
-    });
-    trustedURL = policy.createScriptURL('https://cdnjs.cloudflare.com/ajax/libs/jspdf/1.3.2/jspdf.min.js');
-} else {
-    trustedURL = 'https://cdnjs.cloudflare.com/ajax/libs/jspdf/1.3.2/jspdf.min.js';
-}
-
-let jspdf = document.createElement("script");
-
-jspdf.onload = function () {
-
-    let pdf = new jsPDF();
-    let elements = document.getElementsByTagName("img");
-    for (let i in elements) {
-        let img = elements[i];
-        console.log("add img ", img);
-        if (!/^blob:/.test(img.src)) {
-            console.log("invalid src");
-            continue;
-        }
-        let can = document.createElement('canvas');
-        let con = can.getContext("2d");
-        can.width = img.width;
-        can.height = img.height;
-        con.drawImage(img, 0, 0, img.width, img.height);
-        let imgData = can.toDataURL("image/jpeg", 1.0);
-        pdf.addImage(imgData, 'JPEG', 0, 0);
-        pdf.addPage();
-    }
-
-    pdf.save("download.pdf");
-};
-
-jspdf.src = 'https://drive.google.com/file/d/10su27mqZi6ipykZ4_5oEIjqWp8MCNn03/view'; /* had to set it like this, because disqus was breaking the link.. */
+jspdf.src = trustedURL;
 document.body.appendChild(jspdf);
