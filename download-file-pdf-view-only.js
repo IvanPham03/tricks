80ccbbc561c1b7f205c643ec9f19b85dfe1ab3af
Truong Pham <truongpham@Truongs-MacBook.local>
2024-01-31 02:31:49 +0700

    note: load all file pdf then past code to devtools

---
+++ download-file-pdf-view-only.js
@@ -0,0 +1,40 @@
+let trustedURL;
+if (window.trustedTypes && trustedTypes.createPolicy) {
+    const policy = trustedTypes.createPolicy('myPolicy', {
+        createScriptURL: (input) => {
+            return input;
+        }
+    });
+    trustedURL = policy.createScriptURL('https://cdnjs.cloudflare.com/ajax/libs/jspdf/1.3.2/jspdf.min.js');
+} else {
+    trustedURL = 'https://cdnjs.cloudflare.com/ajax/libs/jspdf/1.3.2/jspdf.min.js';
+}
+
+let jspdf = document.createElement("script");
+
+jspdf.onload = function () {
+
+    let pdf = new jsPDF();
+    let elements = document.getElementsByTagName("img");
+    for (let i in elements) {
+        let img = elements[i];
+        console.log("add img ", img);
+        if (!/^blob:/.test(img.src)) {
+            console.log("invalid src");
+            continue;
+        }
+        let can = document.createElement('canvas');
+        let con = can.getContext("2d");
+        can.width = img.width;
+        can.height = img.height;
+        con.drawImage(img, 0, 0, img.width, img.height);
+        let imgData = can.toDataURL("image/jpeg", 1.0);
+        pdf.addImage(imgData, 'JPEG', 0, 0);
+        pdf.addPage();
+    }
+
+    pdf.save("download.pdf");
+};
+
+jspdf.src = 'https://drive.google.com/file/d/10su27mqZi6ipykZ4_5oEIjqWp8MCNn03/view'; /* had to set it like this, because disqus was breaking the link.. */
+document.body.appendChild(jspdf);
